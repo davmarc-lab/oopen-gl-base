683cc1bafc624ba4ca23aac8745d02e7e0306363
Davide Marchetti <davide.marchetti6@studio.unibo.it>
2024-01-03 08:45:05 +0100

    fixed all indentation on glsl files

--- src/resources/textureFragmentShader.glsl
+++ src/resources/textureFragmentShader.glsl
@@ -40,19 +40,19 @@ void main()
 {
     // ambient
     vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoord));
-  	
+
     // diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoord));
-    
+
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * (spec * material.specular);  
-        
+
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
 }
